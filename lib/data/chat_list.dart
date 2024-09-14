138a32e092c7d1345adf3a8ddd07d06da09d6eb4
YasmineRaef <yasmineraef76@gmail.com>
2024-09-14 11:45:01 -0400

    Editing the chat screen

--- lib/data/chat_list.dart
+++ lib/data/chat_list.dart
@@ -3,11 +3,17 @@ import 'package:flutter/material.dart';
 import '../presentation/resources/app_assets.dart';
 import '../presentation/widgets/chat_bubbles.dart';
 
-List chat = [
+List msgSent = [
+  const ChatMessageBubble(message: "Hi"),
+  const ChatImageBubble(imagePath: AppAssets.lettuceProfile),
+  const ChatMessageBubble(message: "Nice to meet you too!!"),
+];
+
+List msgGot = [
   const ChatMessageBubble(message: "Hello!!"),
   const ChatImageBubble(imagePath: AppAssets.lettuceProfile),
   const ChatMessageBubble(message: "Nice to meet you!!"),
-  const ChatMessageBubble(message: "Testing")
+  const ChatMessageBubble(message: "Hello i am happy that it did work but with the row nested in another row not as we wanted using a wrap widget")
 ];
 
-void addMessage(Widget value) => chat.add(value);
+void addMessage(Widget value) => msgSent.add(value);
